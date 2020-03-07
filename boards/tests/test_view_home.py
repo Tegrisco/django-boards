a9960f48f1f284c9248570699a0c5c3a1e022b86
zzh <zzh1243@163.com>
2020-03-08 00:08:04 +0800

    Initial commit

---
+++ boards/tests/test_view_home.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+
+# Create your tests here.
+from django.core.urlresolvers import reverse
+from django.urls import resolve
+from ..views import BoardListView
+from ..models import Board
+
+class HomeTests(TestCase):
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='Djangp board.')
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_home_view_status_code(self):
+        #url = reverse('home')
+        #response = self.client.get(url)
+        self.assertEquals(self.response.status_code, 200)
+    
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEquals(view.func.view_class, BoardListView)
+
+    def test_home_url_contains_link_to_topics_page(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
+        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
+
+
